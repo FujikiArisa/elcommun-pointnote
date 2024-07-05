ac6b64bc15801e08aab19c321dd2e4d265093c41
FujikiArisa <161689911+FujikiArisa@users.noreply.github.com>
2024-07-05 14:43:36 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.menu-btn').on('click', function(){
+      $('.menu').toggleClass('is-active');
+    });
+  }());
+//   ハンバーガーメニューを押すとメニューを表示
+
+
+//   $(function(){
+//     $('.menu-btn').on('click', function(){
+//       $('.menu-btn').toggleClass('is-active');
+//     });
+//   }());
